c46957e28e18203cbd0cc03513250d9d2fcb3676
Nicolas Fernandez <Nickogabii@gmail.com>
2023-07-12 16:24:11 -0300

    creadas faq y abaout con startapp

--- project/apps/About/views.py
+++ project/apps/About/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 def home(request):
     contexto = {"app": "About" }
     return render(request, "about/index.html", contexto)
--- project/apps/faq/views.py
+++ project/apps/faq/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 def home(request):
     contexto = {"app": "faq" }
     return render(request, "faq/index.html", contexto)
