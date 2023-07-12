a22d6e8cf4a86e063cc5cdf2af732ab1d94ed4ee
Nicolas Fernandez <Nickogabii@gmail.com>
2023-07-12 18:01:11 -0300

    w

--- project/apps/About/views.py
+++ project/apps/About/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-
 def home(request):
     contexto = {"app": "about"}
     return render(request, "about/index.html", contexto)
--- project/apps/faq/views.py
+++ project/apps/faq/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def home(request):
-    contexto = {"app": "faq" }
+    contexto = {"app": "faq"}
     return render(request, "faq/index.html", contexto)
